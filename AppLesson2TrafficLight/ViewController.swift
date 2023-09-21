650e6f646372f522d16fa180141a4d241c700b00
glebzo01 <umnik06@gmail.com>
2023-09-21 22:38:35 +0300

    Initial Commit

---
+++ AppLesson2TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLesson2TrafficLight
+//
+//  Created by Глеб Зобнин on 21.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
